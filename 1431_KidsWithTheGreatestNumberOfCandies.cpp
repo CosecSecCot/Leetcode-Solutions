fa45e10b870eb0996f74ae237be43bf9148ce2e8
CosecSecCot <cosecseccot581@gmail.com>
2023-11-21 20:39:46 +0530

    Done problem-1431

---
+++ 1431_KidsWithTheGreatestNumberOfCandies.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <climits>
+using namespace std;
+#define ll long long
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies) {
+        int max_candies = candies[1];
+        vector<bool> ans;
+
+        for (int i = 0; i < candies.size(); i++) {
+            max_candies = max(max_candies, candies[i]);
+        }
+
+        for (auto candy : candies) {
+            if (candy + extraCandies >= max_candies)
+                ans.push_back(true);
+            else
+                ans.push_back(false);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    return 0;
+}
